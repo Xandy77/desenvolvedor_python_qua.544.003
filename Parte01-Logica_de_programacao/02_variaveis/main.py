a55822a7aafd930fbe378541708b2bdaeb699771
xandy77 <xandnho2017@gmail.com>
2026-07-01 21:18:33 -0300

    Primeiro commit. Aula do dia 01/07/2026.

---
+++ Parte01-Logica_de_programacao/02_variaveis/main.py
@@ -0,0 +1,11 @@
+# declaração de Variáveis
+nome = "Valdomiro Pereira"
+idade = 39
+altura = 1.65
+programador = True
+
+# saída de dados
+print(nome)
+print(idade)
+print(altura)
+print(programador)
---
+++ Parte01-Logica_de_programacao/03_entrada/main.py
@@ -0,0 +1,9 @@
+# declaração de variáveis
+nome = input("Informe seu nome: ")
+idade = input("Informe a sua idade: ")
+altura = input("Informe a sua altura: ")
+
+# saída de dados
+print(nome)
+print(idade)
+print(altura)
