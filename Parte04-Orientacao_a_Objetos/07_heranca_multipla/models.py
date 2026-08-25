3413be31afa7e2e3fb62a34b1bb931bcf1274683
valdomiropereira <xandnho2017@gmail.com>
2026-08-25 19:28:37 -0300

    Aula do dia 25/08/2026.

---
+++ Parte04-Orientacao_a_Objetos/07_heranca_multipla/models.py
@@ -0,0 +1,9 @@
+class Pai:
+    def __init__(self, nome, cpf, email, telefone, profissao):
+        self.nome = nome
+        self.cpf = cpf
+        self.email = email
+        self.telefone = telefone
+        self.profissao = profissao
+
+    
