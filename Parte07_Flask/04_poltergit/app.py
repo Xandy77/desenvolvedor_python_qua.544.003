dc7998fe58c3dbdd0fbdaa7e4999b5fdcb9770ed
Xandy77 <xandnho2017@gmail.com>
2026-09-17 21:48:50 -0300

    Commit automtico - 17/09/2026

--- Parte07_Flask/04_poltergit/app.py
+++ Parte07_Flask/04_poltergit/app.py
@@ -1,10 +1,20 @@
 from flask import Flask, render_template, request 
 import pyautogui as auto
+import webview
 
 from datetime import date
+import os
+import sys
 
 
+if getattr(sys, 'frozen', False):
+    # Se o aplicativo estiver empacotado com PyInstaller
+    template_folder = os.path.join(sys._MEIPASS, 'templates')
 
+    app = Flask(__name__, template_folder=template_folder)
+else:
+    app = Flask(__name__)
+    
 app = Flask(__name__)
 
 @app.route('/')
@@ -33,10 +43,16 @@ def commitar():
         auto.press("enter")
         auto.sleep(3)
         auto.write("exit")
-        auto.press("enter")
-    
+        auto.press("enter")    
     return render_template("index.html")
 
-
 if __name__ == '__main__':
-    app.run(debug=True)
+   # app.run(debug=True)
+   window = webview.create_window(
+        title = "PolterGit",
+        url = app,
+        width = 1000,
+        height = 700
+    )
+   
+   webview.start()
