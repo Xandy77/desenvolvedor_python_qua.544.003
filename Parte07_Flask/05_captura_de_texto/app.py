3e14145148c980a96da88296d49f779815bca024
Xandy77 <xandnho2017@gmail.com>
2026-09-18 21:40:33 -0300

    Commit automtico - 18/09/2026

--- Parte07_Flask/05_captura_de_texto/app.py
+++ Parte07_Flask/05_captura_de_texto/app.py
@@ -1,12 +1,15 @@
 from flask import Flask, render_template, request
 
 
-
 app = Flask(__name__)
 
-@app.route('/')
+@app.route("/")
 def index():
-    return render_template('index.html')
+    return render_template("index.html")
+
+@app.route("/textoExtraído")
+def extrair_texto():
+    return render_template("extracao.html")
 
 if __name__ == '__main__':
     app.run(debug=True)
